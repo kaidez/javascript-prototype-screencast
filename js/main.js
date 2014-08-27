85df7bec82bbf5957f725c8d8491a921def51da5
Kai Gittens <kai.gittens@gmail.com>
2014-08-27 18:24:16 -0400

    update prototype code in js/main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
 // Concrete Class: is never instantiated.
-function Blueprint(){
-  this.name = "N/A";
+function Blueprint(cfg) {
+  this.houseType = cfg.houseType;
+  this.price = cfg.price;
 };
 
-Blueprint.prototype.buildPool = function() {
-     
+Blueprint.prototype.createPool = function(pool) {
+  var thePool = document.querySelector("#pool");
+  this.pool = pool;
+  if(this.pool === undefined) {
+    thePool.innerHTML = "no";
+  }  else {
+    thePool.innerHTML = this.pool;
+  }
 }
+
+var kai = new Blueprint({
+  houseType: "Bunglaow",
+  price: 100,
+});
+kai.createPool("9 ft");
