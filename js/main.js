21f2d2ce604387770197a4d1af1ea9e0934bfe92
kaidez <kai.gittens@gmail.com>
2014-09-05 15:02:22 -0400

    update js/main.js

--- js/main.js
+++ js/main.js
@@ -1,26 +1,49 @@
 // Abstract Class: is never instantiated.
-function Blueprint(lotNumber) {
+function Blueprint( lotNumber ) {
   this.lotNumber = lotNumber;
-  this.totalRooms = ["bedrooms", "bathrooms"];
+
+  if (!this.lotNumber) {
+    this.lotNumber = 0;
+  }
+
+  this.totalBedrooms = 1;
+  this.totalBathrooms = 1.5;
+  this.totalFloors = 1;
   this.basement = this.livingRoom = this.kitchen = this.diningRoom = true;
   this.houseType = "Bungalow";
 }
 
 // Method that adds house options
-Blueprint.prototype.setHomeOptions = function(config) {
+Blueprint.prototype.setHomeOptions = function( config ) {
 
   config = config || {};
   this.counterTops = config.counterTops === undefined ? "formica" : config.counterTops;
-  this.floors = config.floors === undefined ? "tile" : config.floors;
-  this.houseType = config.houseType;
+  this.floorType = config.floorType === undefined ? "tile" : config.floorType;
+  this.houseType = config.houseType || this.houseType;
 
-  
 }
 
-var bungalowOne = new Blueprint(23);
 
-bungalowOne.setHomeOptions({
+
+function Colonial ( lotNumber, windowTypes ) {
+  this.base = Blueprint;
+  this.base( lotNumber, windowTypes );
+  this.windowTypes = windowTypes || "Double pane";
+}
+
+Colonial.prototype = new Blueprint();
+
+
+
+
+
+
+
+var kai = new Colonial( 50, "Single Pane" );
+
+kai.setHomeOptions({
+  floorType: "Hardwood",
   houseType: "Colonial"
 });
 
-console.log(bungalowOne);
+console.log(kai);
