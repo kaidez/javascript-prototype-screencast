c8b97875f018e41842735e8bdfb681478eff7b64
kaidez <kai.gittens@gmail.com>
2014-09-05 10:42:29 -0400

    update js/main.js

--- js/main.js
+++ js/main.js
@@ -1,29 +1,26 @@
 // Abstract Class: is never instantiated.
-function Blueprint(lotNumber, houseType, totalRooms, basement) {
+function Blueprint(lotNumber) {
   this.lotNumber = lotNumber;
-  this.houseType = houseType;
   this.totalRooms = ["bedrooms", "bathrooms"];
-  this.basement = basement;
-  
-  // If basement is not passed as a parameter, set it to false
-  if(!basement) {
-    this.basement = false;
-  }
-  this.livingRoom = true;
-  this.kitchen = true;
-  this.diningRoom = true;
+  this.basement = this.livingRoom = this.kitchen = this.diningRoom = true;
+  this.houseType = "Bungalow";
 }
 
 // Method that adds house options
-Blueprint.prototype.setOptions = function(config) {
+Blueprint.prototype.setHomeOptions = function(config) {
 
   config = config || {};
   this.counterTops = config.counterTops === undefined ? "formica" : config.counterTops;
   this.floors = config.floors === undefined ? "tile" : config.floors;
+  this.houseType = config.houseType;
+
+  
 }
 
-var bungalowOne = new Blueprint(23, "bungalow");
+var bungalowOne = new Blueprint(23);
 
-bungalowOne.setOptions();
+bungalowOne.setHomeOptions({
+  houseType: "Colonial"
+});
 
 console.log(bungalowOne);
