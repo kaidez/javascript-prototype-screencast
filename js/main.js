7b32d445b622bd9c87d77c8b9a8d725f74bfd39b
Kai Gittens <kai.gittens@gmail.com>
2014-09-06 09:05:44 -0400

    update js/main.js...more detailed commit messages start after this

--- js/main.js
+++ js/main.js
@@ -1,49 +1,71 @@
 // Abstract Class: is never instantiated.
-function Blueprint( lotNumber ) {
-  this.lotNumber = lotNumber;
+function Blueprint( lotID ) {
 
-  if (!this.lotNumber) {
-    this.lotNumber = 0;
-  }
+  // Define a unique ID for the home...the only required parameter
+  this.lotID = lotID;
 
+  /*
+   * Default parameters that can be over-ridden, but "basement",
+   * livingRoom", "kitchen" and "diningRoom" do not get over-ridden
+   * because all homes have those.
+   */
+  this.basement = this.livingRoom = this.kitchen = this.diningRoom =
+      true;
+  this.houseType = "Bungalow";
   this.totalBedrooms = 1;
   this.totalBathrooms = 1.5;
   this.totalFloors = 1;
-  this.basement = this.livingRoom = this.kitchen = this.diningRoom = true;
-  this.houseType = "Bungalow";
+
 }
 
-// Method that adds house options
+// Adds home options
 Blueprint.prototype.setHomeOptions = function( config ) {
-
   config = config || {};
   this.counterTops = config.counterTops === undefined ? "formica" : config.counterTops;
+  this.squareFeet = config.squareFeet === undefined ? 1800 : config.squareFeet;
   this.floorType = config.floorType === undefined ? "tile" : config.floorType;
-  this.houseType = config.houseType || this.houseType;
+  this.pool = config.pool === undefined ? false : config.pool;
+  // this.houseType = config.houseType || this.houseType;
 
 }
 
+// Display home options on index.html
+Blueprint.prototype.showHomeOptions = function() {
 
+  // Reference to the "#allHomes" element that's in "index.html"
+  var allHomes = document.getElementById("allHomes");
 
-function Colonial ( lotNumber, windowTypes ) {
+  for (homeOption in this) {
+    if(typeof this[homeOption] !== "function") {
+      console.log(homeOption + ": " + this[homeOption]);
+    }
+  }
+}
+
+function Colonial ( lotID, windowTypes ) {
   this.base = Blueprint;
-  this.base( lotNumber, windowTypes );
+  this.base( lotID, windowTypes );
   this.windowTypes = windowTypes || "Double pane";
+  this.houseType = "Colonial"; 
 }
 
 Colonial.prototype = new Blueprint();
 
 
 
+var niko = new Blueprint(4);
 
+niko.setHomeOptions();
 
-
-
-var kai = new Colonial( 50, "Single Pane" );
+var kai = new Colonial( 50 );
 
 kai.setHomeOptions({
   floorType: "Hardwood",
-  houseType: "Colonial"
+  squareFeet: 2100
 });
+kai.showHomeOptions();
 
-console.log(kai);
+
+
+
+// console.log(kai);
