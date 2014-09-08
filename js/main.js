9231469a6f41ec7b96c7294c41b329d0b84c2403
Kai Gittens <kai.gittens@gmail.com>
2014-09-08 06:20:00 -0400

    update js/main.js

--- js/main.js
+++ js/main.js
@@ -25,37 +25,46 @@ Blueprint.prototype.setHomeOptions = function( config ) {
   this.pool = config.pool === undefined ? "no" : config.pool;
   this.price = config.price || "100,000";
   this.pool = config.pool || "no";
+  this.totalBedrooms = config.totalBedrooms === undefined ? 1 : config.totalBedrooms;
 
   return this;
+
 }
 
 // Display home options on index.html
 Blueprint.prototype.showHomeOptions = function() {
-
   // Reference to the "#allHomes" element that's in "index.html"
   var allHomes = document.getElementById("allHomes"),
-       article = document.createElement("article"),
-       ul = document.createElement("ul");
+      frag = document.createDocumentFragment(),
+      article = document.createElement("article"),
+      ul = document.createElement("ul");
       
   article.appendChild(ul);
   $(article).addClass("col-md-4");
-
   for (homeOption in this) {
+
     if ( typeof this[homeOption] !== "function" && typeof this[homeOption] !== "boolean" ) {
       if ( homeOption === "lotID" ) {
-        // document.getElementById("lotID").innerHTML = "Lot ID: " + this[homeOption];
+        var kai = this[homeOption]
+        $(article).attr("id", kai);
+        // document.getElementById(homeOption).innerHTML = "Lot ID: " + this[homeOption];
       } else {
-         li = document.createElement("li");
-        $(li).addClass( homeOption );
+        var listID = homeOption;
+        listID.toString();
+        console.log(listID + " is");
+        li = document.createElement("li");
+        $(li).attr("id", homeOption);
         ul.appendChild( li );
-        $("." + homeOption ).html( homeOption + ": " + this[homeOption] );
+        $("#" + homeOption ).html( homeOption + ": " + this[homeOption] );
       }
     }
     article.appendChild(ul);
-    allHomes.appendChild(article); 
+    frag.appendChild(article)
+    allHomes.appendChild(frag); 
   }
 
   return this;
+
 }
 
 function Colonial ( lotID, windowTypes ) {
@@ -63,6 +72,7 @@ function Colonial ( lotID, windowTypes ) {
   this.base( lotID, windowTypes );
   this.windowTypes = windowTypes || "Double pane";
   this.houseType = "Colonial";
+  // console.log(this);
 }
 
 Colonial.prototype = new Blueprint();
@@ -70,6 +80,13 @@ Colonial.prototype = new Blueprint();
 var kai = new Colonial( 50 );
 var niko = new Blueprint( 150 );
 
+
 kai.setHomeOptions({
-  floorType: "Marble"
-).showHomeOptions();
+  price: "250000",
+  totalBedrooms: 2
+}).showHomeOptions();
+
+
+niko.setHomeOptions({
+  price: "200000"
+}).showHomeOptions();
