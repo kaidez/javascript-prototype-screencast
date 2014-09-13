835f4589cfdc1c1a87064070822ffb27951632cd
Kai Gittens <kai.gittens@gmail.com>
2014-09-13 19:32:12 -0400

    update main.js in originalSourceCode and startingCode

--- startingCode/js/main.js
+++ startingCode/js/main.js
@@ -1,3 +1,13 @@
-function Blueprint() {
+function Blueprint(lotID) {
 
+  // Define a unique ID
+  this.lotID = lotID;
+
+  this.basement = this.livingRoom = this.kitchen = this.diningRoom =
+      true;
+
+  this.squareFeet = 1000;
+  this.totalBedrooms = 1;
+  this.totalBathrooms = 1.5;
+  this.totalFloors = 1;
 }
