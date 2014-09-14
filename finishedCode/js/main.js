aedce27f545f7b9d9d4a8517d69ead100ce02a92
Kai Gittens <kai.gittens@gmail.com>
2014-09-14 08:36:30 -0400

    globally add comments to main.js

--- finishedCode/js/main.js
+++ finishedCode/js/main.js
@@ -3,13 +3,13 @@ function Blueprint(homeID) {
   // Define a unique ID...the only parameter
   this.homeID = homeID;
   
-  // Common parts of a house...this won't ever change
+  // Common parts of a home...these won't ever change
   this.basement = true;
   this.livingRoom = true;
   this.kitchen = true;
   this.diningRoom = true;
 
-  // Common parts of a house...this won't ever change
+  // Configurable parts of a home...these will change
   this.squareFeet = 1000;
   this.totalBedrooms = 1;
   this.totalBathrooms = 1.5;
