1eb336caf21ea6f456bda2c73d631380871f26e1
Kai Gittens <kai.gittens@gmail.com>
2014-09-27 05:35:54 -0400

    update starting js code in"startingCode/js/main.js"

--- startingCode/js/main.js
+++ startingCode/js/main.js
@@ -1,16 +1,3 @@
-function Blueprint(homeID) {
+function Blueprint() {
 
-  // Define a unique ID...the only parameter
-  this.homeID = homeID;
-  
-  // Common parts of a home...these won't ever change
-  this.basement = this.livingRoom = this.kitchen = this.diningRoom = true;
-
-  // Configurable parts of a home...these will change
-  this.squareFeet = 1000;
-  this.totalBedrooms = 1;
-  this.totalBathrooms = 1.5;
-  this.totalFloors = 1;
 }
-
-var test = new Blueprint(43252345);
