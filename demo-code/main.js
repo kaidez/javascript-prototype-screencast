5c65bac7efa8c6b80ecd1d78791e8db9ecd6a4bf
kai gittens <kai.gittens@gmail.com>
2016-04-17 10:59:18 -0400

    update demo code

--- demo-code/main.js
+++ demo-code/main.js
@@ -1,25 +1,32 @@
-function Blueprint( lotID ){
+function Blueprint( lotID ) {
     this.lotID = lotID;
 }
 
-Blueprint.prototype.name = "Bungalow";
-Blueprint.prototype.showName = function(){
-    console.log(this.name)
+Blueprint.prototype.name = "Regular House";
+
+Blueprint.prototype.showName = function( name ){
+    console.log( name )
 }
+
 var foo = new Blueprint( 123 );
 
-foo.showName();
+foo.showName( "Regular House");
 
-console.log("foo", foo);
+console.log(foo);
 
 function Bungalow(lotID){
-    Blueprint.call(this, lotID)
+    Blueprint.call(this, lotID );
 }
+
+console.log("Blueprint prototype before", Bungalow.prototype);
+
+Bungalow.prototype = Object.create( Blueprint.prototype );
+// Bungalow.prototype = new Blueprint();
+console.log("Blueprint prototype after", Bungalow.prototype);
+
 
 var bar = new Bungalow( 456 );
 console.log("bar", bar);
-foo.showName();
-
-console.log(Blueprint.prototype);
+bar.showName( "Bungalow" );
 
 
