96e38959162744516453897be7724637064159f9
Kai Gittens <kai.gittens@gmail.com>
2016-04-26 06:16:16 -0400

    update demo code

--- demo-code/main.js
+++ demo-code/main.js
@@ -2,32 +2,30 @@ function Blueprint( lotID ) {
     this.lotID = lotID;
 }
 
-Blueprint.prototype.name = "Regular House";
+Blueprint.prototype.type = "Regular House";
 
-Blueprint.prototype.showName = function( name ) {
-    console.log( "This is house is called " + name )
+Blueprint.prototype.showType = function( type ) {
+    console.log( "This is house is called " + type.toUpperCase() );
 }
 
 var newHouse = new Blueprint( 123 );
 
-newHouse.showName( "Regular House");
+newHouse.showType( "Regular House" );
 
-function Bungalow( lotID ) {
-    Blueprint.call( this, lotID );
-}
+// function Bungalow( lotID ) {
+//     Blueprint.call( this, lotID );
+// }
 
-console.log( "Blueprint prototype before" );
-console.log( Bungalow.prototype );
-Bungalow.prototype = Object.create( Blueprint.prototype );
+// console.log( "Bungalow prototype before" );
+// console.log( Bungalow.prototype );
+// Bungalow.prototype = Object.create( Blueprint.prototype );
 // Bungalow.prototype = new Blueprint();
 
-console.log( "\n" );
-console.log( "Blueprint prototype after:" );
-console.log( Bungalow.prototype );
+// console.log( "\n" );
+// console.log( "Bungalow prototype after:" );
+// console.log( Bungalow.prototype );
 
 
-var bar = new Bungalow( 456 );
-console.log( "bar is ", bar );
-bar.showName( "Bungalow" );
-
-
+// var bar = new Bungalow( 456 );
+// console.log( "bar is ", bar );
+// bar.showType( "Bungalow" );
